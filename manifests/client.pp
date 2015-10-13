0f444920701d2645cda656c4f3ae59d0dedeb274
Baptiste Grenier <bgrenier@gnubila.fr>
2015-10-13 10:19:15 +0200

    nfs::client: use full namespace

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,8 +1,9 @@
 class nfs::client {
   case $::osfamily {
-    'Debian': { include nfs::client::debian}
-    'RedHat': { include nfs::client::redhat}
+    'Debian': { include ::nfs::client::debian}
+    'RedHat': { include ::nfs::client::redhat}
     default:  { fail "Unsupported OS familly ${::osfamily}" }
   }
 }
 
+# vim: set et sta sw=2 ts=2 sts=2 noci noai:
