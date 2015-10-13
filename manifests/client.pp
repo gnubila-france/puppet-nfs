50b14a39c1f97a11efad06ebb5395a39b64cb243
Baptiste Grenier <bgrenier@gnubila.fr>
2015-10-13 13:20:02 +0200

    Style.

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,7 +1,7 @@
 class nfs::client {
   case $::osfamily {
-    'Debian': { include ::nfs::client::debian}
-    'RedHat': { include ::nfs::client::redhat}
+    'Debian': { include ::nfs::client::debian }
+    'RedHat': { include ::nfs::client::redhat }
     default:  { fail "Unsupported OS familly ${::osfamily}" }
   }
 }
