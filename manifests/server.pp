d6a3d91233ba1ffcbe2155da0ce36276599a4f93
Jerome Revillard <jrevillard@gnubila.fr>
2016-06-17 09:01:31 +0200

    Server: client_ips variable is now an array

--- manifests/server.pp
+++ manifests/server.pp
@@ -2,13 +2,13 @@
 #
 # Role for installing a NFS Server
 #
-# one of the 2 parameters must be set
+# one of the 2 parameters must be set.
 #
 # === Parameters
 #
 # [*client_ips*]
-#   client IP or list of client IPs séparated by ',' .
-#   Default: undef
+#   Array of client IPs.
+#   Default: []
 #
 # [*client_ips_range*]
 #   range of client IPs.
@@ -22,83 +22,83 @@
 #
 #
 class nfs::server (
-  $client_ips = undef,
+  $client_ips = [],
   $client_ips_range = undef,
 )
 {
+  validate_array($client_ips)
 
-  if $client_ips == undef and $client_ips_range == undef {
-    fail('One of client_ips or client_ips_range should be provided!')
-  }
-  if $client_ips != undef and $client_ips_range != undef {
-    fail('Only one of client_ip and client_ips_range should be provided!')
+  if size($client_ips) == 0 and $client_ips_range == undef {
+    fail('One of client_ips array or client_ips_range should be provided!')
   }
 
-  if $client_ips != undef {
-    firewall { '050 allow UDP rpcbind port':
-      source => $client_ips,
-      proto  => 'udp',
-      dport  => '111',
-      action => 'accept',
-    }
-    firewall { '050 allow TCP rpcbind port':
-      source => $client_ips,
-      dport  => '111',
-      action => 'accept',
-    }
-    firewall { '050 allow TCP NFS port':
-      source => $client_ips,
-      dport  => '2049',
-      action => 'accept',
-    }
-    firewall { '050 allow TCP LOCKD_TCPPORT port':
-      source => $client_ips,
-      dport  => '32803',
-      action => 'accept',
-    }
-    firewall { '050 allow TCP callback port':
-      source => $client_ips,
-      dport  => '32764',
-      action => 'accept',
-    }
-    firewall { '050 allow UDP LOCKD_UDPPORT port':
-      source => $client_ips,
-      proto  => 'udp',
-      dport  => '32769',
-      action => 'accept',
-    }
-    firewall { '050 allow UDP MOUNTD_PORT port':
-      source => $client_ips,
-      proto  => 'udp',
-      dport  => '892',
-      action => 'accept',
-    }
-    firewall { '050 allow TCP MOUNTD_PORT port':
-      source => $client_ips,
-      dport  => '892',
-      action => 'accept',
-    }
-    firewall { '050 allow UDP RQUOTAD_PORT port':
-      source => $client_ips,
-      proto  => 'udp',
-      dport  => '875',
-      action => 'accept',
-    }
-    firewall { '050 allow TCP RQUOTAD_PORT port':
-      source => $client_ips,
-      dport  => '875',
-      action => 'accept',
-    }
-    firewall { '050 allow UDP STATD_PORT port':
-      source => $client_ips,
-      proto  => 'udp',
-      dport  => '662',
-      action => 'accept',
-    }
-    firewall { '050 allow TCP STATD_PORT port':
-      source => $client_ips,
-      dport  => '662',
-      action => 'accept',
+  if size($client_ips) > 0 {
+    $client_ips.each |String $client_ip| {
+      firewall { "050 allow UDP rpcbind port  from $client_ip":
+        source => $client_ip,
+        proto  => 'udp',
+        dport  => '111',
+        action => 'accept',
+      }
+      firewall { "050 allow TCP rpcbind port from $client_ip":
+        source => $client_ip,
+        dport  => '111',
+        action => 'accept',
+      }
+      firewall { "050 allow TCP NFS port from $client_ip":
+        source => $client_ip,
+        dport  => '2049',
+        action => 'accept',
+      }
+      firewall { "050 allow TCP LOCKD_TCPPORT port from $client_ip":
+        source => $client_ip,
+        dport  => '32803',
+        action => 'accept',
+      }
+      firewall { "050 allow TCP callback port from $client_ip":
+        source => $client_ip,
+        dport  => '32764',
+        action => 'accept',
+      }
+      firewall { "050 allow UDP LOCKD_UDPPORT port from $client_ip":
+        source => $client_ip,
+        proto  => 'udp',
+        dport  => '32769',
+        action => 'accept',
+      }
+      firewall { "050 allow UDP MOUNTD_PORT port from $client_ip":
+        source => $client_ip,
+        proto  => 'udp',
+        dport  => '892',
+        action => 'accept',
+      }
+      firewall { "050 allow TCP MOUNTD_PORT port from $client_ip":
+        source => $client_ip,
+        dport  => '892',
+        action => 'accept',
+      }
+      firewall { "050 allow UDP RQUOTAD_PORT port from $client_ip":
+        source => $client_ip,
+        proto  => 'udp',
+        dport  => '875',
+        action => 'accept',
+      }
+      firewall { "050 allow TCP RQUOTAD_PORT port from $client_ip":
+        source => $client_ip,
+        dport  => '875',
+        action => 'accept',
+      }
+      firewall { "050 allow UDP STATD_PORT port from $client_ip":
+        source => $client_ip,
+        proto  => 'udp',
+        dport  => '662',
+        action => 'accept',
+      }
+      firewall { "050 allow TCP STATD_PORT port from $client_ip":
+        source => $client_ip,
+        dport  => '662',
+        action => 'accept',
+      }
     }
   } else {
     firewall { '050 allow UDP rpcbind port':
